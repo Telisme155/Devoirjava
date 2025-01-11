54ab93a7fa1b3fcef216eef91d15377130394884
Telisme155 <wilguenstelisme349@gmail.com>
2025-01-10 20:55:35 -0500

    faidherbe

---
+++ function.js
@@ -0,0 +1,25 @@
+
+ 
+
+
+// Exercice #1
+
+
+function Elève(numero1,numero2,numero3, numero4){
+    let total = numero1 + numero2 + numero3 + numero4
+    console.log(total);
+  }
+  Elève(2,4,6,8);
+
+
+
+              //Exercice 2//
+
+    function calculaterectanglearea(longeur , largeur){ 
+        let resultats = longeur * largeur;
+        console.log("resultats is," + resultats);
+     }
+    
+     //let length = 30;
+    // width = 40;
+    calculaterectanglearea(30 , 40);
